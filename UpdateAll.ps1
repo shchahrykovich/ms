b3d4cfd72040aa69b7e4219375a8eb81416d0af0
Ubuntu <shchegrikovich@shchegrikovih-ubuntu-server.ihlolnmxfekefg5a04wia4xome.fx.internal.cloudapp.net>
2016-08-29 16:59:00 +0000

    Fixed linux specific bugs

--- UpdateAll.ps1
+++ UpdateAll.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/powershell
+
 $projects = @("dotnet/coreclr", "dotnet/corefx", "dotnet/corefxlab", "dotnet/corert", 
               "dotnet/cli", "microsoft/msbuild", "dotnet/dotnet-ci", "dotnet/buildtools", 
               "dotnet/roslyn", "dotnet/llilc", "aspnet/dnx", "aspnet/KestrelHttpServer", 
@@ -10,32 +12,39 @@ if ( -not (Test-Path $gitex) ) {
 	$gitex="C:\Program Files\Git\bin\git.exe"
 }
 
+if ( $IsLinux ) {
+  $gitex="git"
+}
+
 # Clone repos
 foreach($p in $projects)
 {
     if ( Test-Path "$PSScriptRoot\$p" ) {
         Write-Output "Updating $p"
-        cd "$PSScriptRoot\$p"
+        cd "$PSScriptRoot/$p"
         .$gitex pull 
         cd $PSScriptRoot
     } else {
         Write-Output "Creating $p"
-        .$gitex clone https://github.com/$p "$PSScriptRoot\$p"
+        .$gitex clone https://github.com/$p "$PSScriptRoot/$p"
     }
     Write-Output "==="
 }
 
+
 # Checkout releases
-$releases = curl https://api.github.com/repos/dotnet/coreclr/tags | % { $_.Content } | ConvertFrom-Json
+if ( -not $IsLinux ) {
+$releases = curl -s https://api.github.com/repos/dotnet/coreclr/tags | % { $_.Content } | ConvertFrom-Json
 $releases = $releases | ?{ $_.name.StartsWith("v") }
 foreach($r in $releases)
 {
     Write-Output "Updating $($r.name)"
-    $path = "$PSScriptRoot\dotnet\coreclr-$($r.name)"
+    $path = "$PSScriptRoot/dotnet/coreclr-$($r.name)"
     if( -not(Test-Path $path) ) {
-  	    .$gitex clone "$PSScriptRoot\dotnet\coreclr" $path
+        .$gitex clone "$PSScriptRoot/dotnet/coreclr" $path
         cd $path
         .$gitex checkout $r.commit.sha
     }
     Write-Output "==="
+}
 }
